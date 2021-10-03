22628792caa8f1c0e4a17dbbe51999ea2c7af996
shahpreet0505 <91820479+shahpreet0505@users.noreply.github.com>
2021-10-03 17:18:04 +0530

    Add files via upload

---
+++ CoinChange.java
@@ -0,0 +1,35 @@
+public class CoinChange {  static int coinchange(int[] a, int v, int n, int[][] dp)
+{
+    if (v == 0)
+        return dp[n][v] = 1;
+    if (n == 0)
+        return 0;
+    if (dp[n][v] != -1)
+        return dp[n][v];
+    if (a[n - 1] <= v)
+    {
+
+        return dp[n][v]
+                = coinchange(a, v - a[n - 1], n, dp)
+                + coinchange(a, v, n - 1, dp);
+    }
+    else // We have no option but to leave this coin
+        return dp[n][v] = coinchange(a, v, n - 1, dp);
+}
+    public static void main(String[] args)
+    {
+        int tc = 1;
+        while (tc != 0) {
+            int n, v;
+            n = 3;
+            v = 4;
+            int[] a = { 1, 2, 3 };
+            int[][] dp = new int[n + 1][v + 1];
+            for (int[] row : dp)
+            Arrays.fill(row, -1);
+            int res = coinchange(a, v, n, dp);
+            System.out.println(res);
+            tc--;
+        }
+    }
+}
